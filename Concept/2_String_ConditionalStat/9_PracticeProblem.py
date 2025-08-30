e9e8adc19c1bd748d397fae2851bb4c98ff1a61d
A-man2611 <57085869+A-man2611@users.noreply.github.com>
2025-08-30 12:15:28 +0530

    Done with Part2

---
+++ Concept/2_String_ConditionalStat/9_PracticeProblem.py
@@ -0,0 +1,6 @@
+# WAP to check if a number is a multile of 7 or not
+multiple = int(input("Enter the number: "))
+if(multiple%7 == 0):
+    print("It's a multiple of 7")
+else:
+    print("It's not multiple of 7")
