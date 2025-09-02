2b80c1ef6d258568f30aa48dc07e356f0751ba5b
A-man2611 <57085869+A-man2611@users.noreply.github.com>
2025-09-02 13:31:56 +0530

    Adding Dict and set portion

---
+++ Concept/4_Dict_Set/1_Dictionary.py
@@ -0,0 +1,12 @@
+# Dict are used to store data in values in KEY:VALUES pairs
+# They are unordered, mutable(changable) & don't allow duplicate keys
+Student_dict = {
+    "Key"  : "value", #strings
+    "name" : "Aman",  #strings  
+    "cgpa" : 9.67,    #strings and float 
+    "marks" : [98.6,100,95],  #strings and float 
+    "is_male" : True,  #strings and bool
+    "Topics" : ("dict", "set"),  #strings and tuple
+    12.33 : 21 #both int
+}
+print(Student_dict)
